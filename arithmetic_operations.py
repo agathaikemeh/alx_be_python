9033a35eeed9db8f019542cfbd452918a1dbfd97
Ikemeh Agatha <ikemehagatha@gmail.com>
2024-09-28 02:16:28 +0100

    Implement arithmetic operations function

--- arithmetic_operations.py
+++ arithmetic_operations.py
@@ -1,15 +1,25 @@
-# arithmetic_operations.py
-
 def perform_operation(num1, num2, operation):
-    if operation == "add":
+    """
+    Perform arithmetic operations based on the operation parameter.
+
+    Parameters:
+    - num1 (float): First number
+    - num2 (float): Second number
+    - operation (str): The operation to perform (add, subtract, multiply, divide)
+
+    Returns:
+    - Result of the operation, or a message in case of division by zero.
+    """
+    if operation == 'add':
         return num1 + num2
-    elif operation == "subtract":
+    elif operation == 'subtract':
         return num1 - num2
-    elif operation == "multiply":
+    elif operation == 'multiply':
         return num1 * num2
-    elif operation == "divide":
+    elif operation == 'divide':
         if num2 == 0:
-            return "Cannot divide by zero!"
+            return "Error: Division by zero is not allowed"
         return num1 / num2
     else:
-        return "Invalid operation"
+        return "Error: Invalid operation"
+
