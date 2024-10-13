cab30882c00eafa6edea22174c875fc21b1d155d
Ikemeh Agatha <ikemehagatha@gmail.com>
2024-10-13 23:04:45 +0100

    Fix implementation of magic methods in Book class

--- oop/book_class.py
+++ oop/book_class.py
@@ -10,11 +10,11 @@ class Book:
     def __del__(self):
         print(f"Deleting {self.title}")
 
-    def __str__(self) -> str:
+    def __str__(self)-> str:
         """String representation of the Book instance."""
         return f"{self.title} by {self.author}, published in {self.year}"
 
-    def __repr__(self) -> str:
+    def __repr__(self)-> str:
         """Official representation of the Book instance."""
         return f"Book('{self.title}', '{self.author}', {self.year})"
 
