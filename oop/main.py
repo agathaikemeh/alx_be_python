9e8bf9615dadf090ff20e4a126494eaba52a8ca9
Ikemeh Agatha <ikemehagatha@gmail.com>
2024-10-23 12:51:30 +0100

    Implemented Book class with magic methods

--- oop/main.py
+++ oop/main.py
@@ -1,18 +1,9 @@
-# main.py
-
 from book_class import Book
 
 def main():
-    # Creating an instance of Book
     my_book = Book("1984", "George Orwell", 1949)
-
-    # Demonstrating the __str__ method
-    print(my_book)  # Expected to use __str__
-
-    # Demonstrating the __repr__ method
-    print(repr(my_book))  # Expected to use __repr__
-
-    # Deleting a book instance to trigger __del__
+    print(my_book)
+    print(repr(my_book))
     del my_book
 
 if __name__ == "__main__":
