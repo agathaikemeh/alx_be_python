a730b1315ab9c91b6ad3e991709f9b4d8aa3f32a
Ikemeh Agatha <ikemehagatha@gmail.com>
2024-10-24 21:20:18 +0100

    Re-added destructor without comments

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,5 +1,5 @@
 class Book:
-    def __init__(self, title: str, author: str, year: int):
+    def __init__(self, title, author, year):
         self.title = title
         self.author = author
         self.year = year
@@ -10,7 +10,7 @@ class Book:
     def __str__(self):
         return f"{self.title} by {self.author}, published in {self.year}"
 
-    def __repr__(self) -> str:
+    def __repr__(self):
         return f"Book('{self.title}', '{self.author}', {self.year})"
 
 
@@ -38,10 +38,10 @@ class Book:
 
 
 
-<<<<<<< HEAD
-=======
 
 
 
 
->>>>>>> cbb108a989269d8513b6b31f2fea0624d0b06c44
+
+
+
